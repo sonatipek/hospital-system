b08ba9328620b9f710d52fe22b4ae6db873bb0d9
Sonat Saygın İPEK <sonatipek55@gmail.com>
2023-06-05 19:37:13 +0300

    file structure has been organized

--- hastalistele.php
+++ hastalistele.php
@@ -143,6 +143,7 @@
     <!-- Bootstrap CDN -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 
+    <!-- Search in the table -->
     <script>
         $(document).ready(function () {
             $("#search-input-lg").on("keyup", function () {
