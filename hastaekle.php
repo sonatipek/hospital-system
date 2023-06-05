15a6a6ad135c8c39bbcaa71e9e84c73de027f42c
Sonat Saygın İPEK <sonatipek55@gmail.com>
2023-06-05 19:37:56 +0300

    patient add feature added

---
+++ hastaekle.php
@@ -0,0 +1,161 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="shortcut icon" href="./src/images/favicon/logoipsum-296.svg" type="image/x-icon">
+    <title>Nesne Tabanlı Programlama Final | Sonat Saygın İpek</title>
+
+    <!-- Bootstrap CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+    <!-- Font Awesome CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <!----------------------------Navbar--------------------------->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Nesne Tabanlı Final">
+        <div class="container">
+            <!-- Brand Name -->
+            <a class="navbar-brand fw-bold" href="./">
+                <img src="./src/images/brand_logo.svg" alt="final exam brand logo">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+    
+            <!-- Navbar Links -->
+            <div class="collapse navbar-collapse ms-5" id="navbarsExample07">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-5">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="./">Ana Sayfa</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./hastalistele.php">Hastalar</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./goruntuyukle.php">Görüntü Yükle</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./goruntulistele.php">Görüntü Listele</a>
+                    </li>
+                </ul>
+
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a href="./hastaekle.php" class="btn btn-outline-primary">Hasta Ekle</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-----------------------------Navbar End---------------------->
+
+    <?php
+        if (isset($_GET)) {
+            # code...
+        }
+    ?>
+
+    <!-- Success Information -->
+    <?php if(isset($_GET['success'])) : ?>
+        <div class="alert alert-success" role="alert">
+            Hasta Ekleme İşlemi Başarılı!
+        </div>
+    <?php endif; ?>
+    <!-- Success Information End -->
+
+    <!-- Unsuccess Information -->
+    <?php if(isset($_GET['unsuccess'])) : ?>
+        <div class="alert alert-danger" role="alert">
+            Hasta Ekleme İşlemi Başarısız!
+        </div>
+    <?php endif; ?>
+    <!-- Unsuccess Information End -->
+
+    <main class="container mx-auto w-25 mt-5">
+        <form method="POST" action="./db/hastakayit.php">
+            <!-- Form Image -->
+            <div class="col-12 text-center">
+                <img class="mb-4" src="./src/images/brand_logo.svg" alt="logo" height="60">
+            </div>
+
+
+            <!-- Form Title -->
+            <h1 class="fs-3 fw-light mt-2">Hasta Ekleme Formu</h1>
+            
+
+            <!-- Hasta Adı ve Soyadı -->
+            <div class="row">
+                <div class="col-6 mb-2">
+                    <label for="hasta_adi" class="form-label">Hasta Adı</label>
+                    <input type="text" class="form-control" id="hasta_adi" name="hasta_adi">
+                </div>
+        
+                <div class="col-6 mb-2">
+                    <label for="hasta_soyadi" class="form-label">Hasta Soyadı</label>
+                    <input type="text" class="form-control" id="hasta_soyadi" name="hasta_soyadi">
+                </div>
+            </div>
+            <!-- Hasta Adı ve Soyadı End -->
+
+
+            <!-- Hasta TC -->
+            <div class="col-12 my-2">
+                <label for="hasta_tc" class="form-label">Hasta T.C. No</label>
+                <input type="number" class="form-control" id="hasta_tc" name="hasta_tc">
+            </div>
+            <!-- Hasta TC End -->
+
+            <!-- Hasta Tel -->
+            <div class="col-12 my-2">
+                <label for="hasta_tel" class="form-label">Hasta Tel. No</label>
+                <input type="number" class="form-control" id="hasta_tel" name="hasta_tel">
+            </div>
+            <!-- Hasta Tel End -->
+    
+            <!-- Hasta Cinsiyet -->
+            <div class="col-md-12 mt-2">
+                <label for="hasta_cinsiyet" class="form-label">Cinsiyet</label>
+            
+                <select class="form-select" id="hasta_cinsiyet" name="hasta_cinsiyet">
+                    <option value="">Lütfen cinsiyetinizi seçiniz</option>
+                    <option value="E">Erkek</option>
+                    <option value="K">Kadın</option>
+                </select>
+            </div>
+    
+    
+            <button class="btn btn-lg btn-primary w-100 mt-4" type="submit">Ekle</button>
+        </form>
+    </main>
+    
+
+    <!------------------------------------Footer------------------------------------>
+    <div class="container">
+        <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+            <p class="col-md-4 mb-0 text-body-secondary">© 2023 Sonat Saygın İpek</p>
+        
+            <a href="./" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+                <b class="fw-bold fs-5">NT Final</b>
+            </a>
+      
+            <!-- Footer Links -->
+            <ul class="nav col-md-4 justify-content-end">
+                <li class="nav-item"><a href="./" class="nav-link px-2 text-body-secondary">Ana Sayfa</a></li>
+                <li class="nav-item"><a href="./hastalistele.php" class="nav-link px-2 text-body-secondary">Hastalar</a></li>
+                <li class="nav-item"><a href="./goruntuyukle.php" class="nav-link px-2 text-body-secondary">Görüntü Yükle</a></li>
+                <li class="nav-item"><a href="./goruntulistele.php" class="nav-link px-2 text-body-secondary">Görüntü Listele</a></li>
+            </ul>
+            
+        </footer>
+    </div>
+    <!------------------------------------Footer  End------------------------------->
+
+    <!-- jQuery CDN -->
+    <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+
+    <!-- Bootstrap CDN -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
+</html>
+
